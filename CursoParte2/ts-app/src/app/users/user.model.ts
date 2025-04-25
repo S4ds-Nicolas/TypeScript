0afbb0823c2e78e7b9171ede65ec980d6a5e0d38
Nicolas Macias Duque <nicolas.macias@s4ds.com>
2025-04-25 09:05:03 -0500

    Avances viernes 25/04

---
+++ CursoParte2/ts-app/src/app/users/user.model.ts
@@ -0,0 +1,13 @@
+import { BaseModel } from "../base.model";
+
+export enum ROLES {
+    ADMIN = "admin",
+    SELLER = "seller",
+    CUSTOMER = "customer"
+}
+
+export interface  User extends BaseModel{
+    username : String;
+    role: ROLES;
+}
+
